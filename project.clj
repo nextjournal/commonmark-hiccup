ac13f68e15d934a21e187f6840039c2de8a066e2
Reid 'arrdem' McKenzie <me@arrdem.com>
2017-12-21 10:21:33 -0800

    Vendor & lein-git-version

--- project.clj
+++ project.clj
@@ -1,8 +1,18 @@
-(defproject commonmark-hiccup "0.1.0"
+(defproject me.arrdem/commonmark-hiccup "_"
   :description "Library to render CommonMark markdown to customizable HTML."
   :url "https://github.com/bitterblue/commonmark-hiccup"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.atlassian.commonmark/commonmark "0.8.0"]
-                 [hiccup "1.0.5"]])
+                 [hiccup "1.0.5"]]
+
+  :plugins [[me.arrdem/lein-git-version "LATEST"]]
+  :git-version {:status-to-version
+                (fn [{:keys [tag version ahead ahead? dirty?] :as git}]
+                  (if (and tag (not ahead?) (not dirty?))
+                    tag
+                    (str tag
+                         (when ahead? (str "." ahead))
+                         (when dirty? "-SNAPSHOT"))))})
